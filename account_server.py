c0c6f77d7b1eb1c089ad09570abcfc8580de962c
mufa <weiqing0213rui@gmail.com>
2024-05-27 22:22:54 +0900

    getuser check

--- account_server.py
+++ account_server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from module import account_manager
+import base64
 
 
 app = Flask(__name__)
@@ -16,8 +17,12 @@ def signup():
 @app.route('/users/<user_id>', methods=['GET', 'PATCH'])
 def getuser(user_id):
     auth_header = request.headers.get('Authorization')
+    auth_token = auth_header.split(' ')[1]
+    auth_decoded = base64.b64decode(auth_token).decode('utf-8')
+    input_userid, input_password = auth_decoded.split(':')
     print('!!!')
-    print(auth_header)
+    print(input_userid)
+    print(input_password)
     print('!!!')
     User_manager = account_manager()
     response = User_manager.userinfo(user_id,
